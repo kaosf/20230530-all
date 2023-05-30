4091af08329aaf67a67bba553782533733e8e81f
ka <ka.kaosf@gmail.com>
2023-05-30 23:58:41 +0900

    Fix to `all(a: &[bool]) -> Result<bool, EmptyError>`

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,42 @@
-fn all(a: &[bool]) -> bool {
+#[derive(Debug)]
+struct EmptyError;
+
+fn all1(a: &[bool]) -> bool {
     if a.is_empty() {
         true
     } else {
-        a[0] && all(&a[1..])
+        a[0] && all1(&a[1..])
+    }
+}
+
+fn all(a: &[bool]) -> Result<bool, EmptyError> {
+    if a.is_empty() {
+        Err(EmptyError)
+    } else {
+        Ok(all1(&a))
     }
 }
 
 #[test]
 fn all_works() {
-    assert_eq!(all(&[]), true);
-    assert_eq!(all(&[true]), true);
-    assert_eq!(all(&[false]), false);
-    assert_eq!(all(&[true, true]), true);
-    assert_eq!(all(&[true, false]), false);
-    assert_eq!(all(&[false, true]), false);
-    assert_eq!(all(&[false, false]), false);
+    assert!(all(&[]).is_err());
+    assert_eq!(all(&[true]).unwrap(), true);
+    assert_eq!(all(&[false]).unwrap(), false);
+    assert_eq!(all(&[true, true]).unwrap(), true);
+    assert_eq!(all(&[true, false]).unwrap(), false);
+    assert_eq!(all(&[false, true]).unwrap(), false);
+    assert_eq!(all(&[false, false]).unwrap(), false);
 }
 
 fn main() {
-    println!("{}", all(&[]));
+    println!(
+        "{}",
+        match all(&[]) {
+            Ok(true) => "True",
+            Ok(false) => "False",
+            Err(EmptyError) => "EmptyError",
+        }
+    );
     println!("{}", [].iter().all(|x| *x));
     println!("Run `cargo t`");
 }
