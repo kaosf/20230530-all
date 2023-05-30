2eb45dae009e2b02639f87b8695acddd5de17e35
ka <ka.kaosf@gmail.com>
2023-05-30 23:53:20 +0900

    Impl `all(a: &[bool]) -> bool`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+fn all(a: &[bool]) -> bool {
+    if a.is_empty() {
+        true
+    } else {
+        a[0] && all(&a[1..])
+    }
+}
+
+#[test]
+fn all_works() {
+    assert_eq!(all(&[]), true);
+    assert_eq!(all(&[true]), true);
+    assert_eq!(all(&[false]), false);
+    assert_eq!(all(&[true, true]), true);
+    assert_eq!(all(&[true, false]), false);
+    assert_eq!(all(&[false, true]), false);
+    assert_eq!(all(&[false, false]), false);
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", all(&[]));
+    println!("{}", [].iter().all(|x| *x));
+    println!("Run `cargo t`");
 }
